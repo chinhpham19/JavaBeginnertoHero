68a18cd6d890a4281fcb992ea27d0cb55f832979
hoc IT with MrChinh <pvc19902@gmail.com>
2024-06-12 16:18:32 +0700

    upload hello world

--- JavatoBeginner/src/HelloWorld.java
+++ JavatoBeginner/src/HelloWorld.java
@@ -1,5 +1,6 @@
-public class App {
+public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("abc");
     }
 }
